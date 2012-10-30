8546935d637ed323998ba30f38c075322b0300d8
zenind <zenind@zeninds-MacBook-Pro.local>
2012-10-31 00:25:36 +0300

    Add logging of test sequence and failed tests

---
+++ src/main/java/com/lazerycode/jmeter/console/ConsoleUtils.java
@@ -0,0 +1,11 @@
+package com.lazerycode.jmeter.console;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zenind
+ * Date: 10/31/12
+ * Time: 12:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConsoleUtils {
+}
