ef6fe528cb49667cdbdc057e765d96ad812626e4
bredish <breedish@gmail.com>
2013-02-04 00:55:30 +0300

    #730471 Initial commit. More tests to follow.

---
+++ jmeter/src/main/java/com/breedish/test/automator/App.java
@@ -0,0 +1,13 @@
+package com.breedish.test.automator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
